843849bab2cf656c54abe060c6b27fe1fe243596
jrosen48 <jmrosen48@gmail.com>
2022-11-29 11:32:52 -0500

    add a POC

--- newdev/server.R
+++ newdev/server.R
@@ -8,6 +8,8 @@ shinyServer(function(input, output) {
          width = "75%")
   }, deleteFile = F)
 
-   
+  output$suggested_analysis <- renderText( {
+    "pkonfound(b = xx, se = xx, df = xx, ncov = xx)"
+  })
 
 })
